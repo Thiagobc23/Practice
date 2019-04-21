eceb84be9092f6d73d3d37dcaf7b3daa10c382cb
Thiago Bernardes Carvalho <thiago.bernardes.carvalho@gmail.com>
2019-04-20 18:52:25 -0700

    Delete myProducts.py

--- Python/SQLite/myProducts.py
+++
@@ -1,9 +0,0 @@
-class Products:
-
-    def __init__(self, name, category, value):
-        self.name = name
-        self.category = category
-        self.value = value
-
-    def __repr__(self):
-        return "Products('{}','{}','{}')".format(self.name, self.category, self.value)
